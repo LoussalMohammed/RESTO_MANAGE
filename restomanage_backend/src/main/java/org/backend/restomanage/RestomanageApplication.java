23919f6ebb4967320717ce778eaed3235f4cb641
loussalmohammed <hplaptopsimo@gmail.com>
2025-01-21 10:05:32 +0100

    add backend spring boot project

---
+++ restomanage_backend/src/main/java/org/backend/restomanage/RestomanageApplication.java
@@ -0,0 +1,13 @@
+package org.backend.restomanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestomanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestomanageApplication.class, args);
+	}
+
+}
---
+++ restomanage_backend/src/test/java/org/backend/restomanage/RestomanageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.restomanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestomanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
