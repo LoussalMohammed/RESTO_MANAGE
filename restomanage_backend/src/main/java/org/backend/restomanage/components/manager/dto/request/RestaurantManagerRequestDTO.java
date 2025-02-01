c16be36bbeba3cc36f17265c51cd0ed924a95b44
HamzaMeski <meskihamza5@gmail.com>
2025-02-01 18:49:09 +0100

    fixing existing issue

--- restomanage_backend/src/main/java/org/backend/restomanage/components/manager/dto/request/RestaurantManagerRequestDTO.java
+++ restomanage_backend/src/main/java/org/backend/restomanage/components/manager/dto/request/RestaurantManagerRequestDTO.java
@@ -22,8 +22,6 @@ public class RestaurantManagerRequestDTO {
     private String phoneNumber;
 
     @NotBlank(message = "Username is required")
-    @Pattern(regexp = "^[a-zA-Z0-9_]{3,20}$", 
-            message = "Username must be between 3 and 20 characters and can only contain letters, numbers, and underscores")
     private String username;
 
     @NotBlank(message = "Password is required")
--- restomanage_backend/src/main/java/org/backend/restomanage/components/settings/mapper/SettingsMapper.java
+++ restomanage_backend/src/main/java/org/backend/restomanage/components/settings/mapper/SettingsMapper.java
@@ -1,5 +1,6 @@
 package org.backend.restomanage.components.settings.mapper;
 
+import org.backend.restomanage.components.manager.mapper.RestaurantManagerMapper;
 import org.backend.restomanage.components.settings.dto.request.BusinessHoursRequestDTO;
 import org.backend.restomanage.components.settings.dto.request.RestaurantSettingsRequestDTO;
 import org.backend.restomanage.components.settings.dto.response.BusinessHoursResponseDTO;
@@ -10,7 +11,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {RestaurantManagerMapper.class})
 public interface SettingsMapper {
     @Mapping(target = "id", ignore = true)
     BusinessHours toEntity(BusinessHoursRequestDTO dto);
@@ -19,8 +20,13 @@ public interface SettingsMapper {
     void updateBusinessHoursFromDTO(BusinessHoursRequestDTO dto, @MappingTarget BusinessHours entity);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "restaurantManager", ignore = true)
     RestaurantSettings toEntity(RestaurantSettingsRequestDTO settingsRequestDTO);
+
+    @Mapping(target = "manager", source = "restaurantManager")
     RestaurantSettingsResponseDTO toDTO(RestaurantSettings settings);
+
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "restaurantManager", ignore = true)
     void updateSettingsFromDTO(RestaurantSettingsRequestDTO settingsRequestDTO, @MappingTarget RestaurantSettings settings);
 }
